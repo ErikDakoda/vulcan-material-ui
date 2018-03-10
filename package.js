4745d3cb645ead13f0e516527e208fbc6aaa2c40
Erik Schannen <erikschannen@gmail.com>
2018-03-10 14:36:30 -0500

    * Added support for platform apps - integrations using apps to be installed on target platforms  * Created a Shopify App  * Added support for obtaining OAuth tokens via the Shopify App and forwarding those tokens for use by SMCommerce  * Modified the behaviour of EcomApiUrl, EcomPlatformsSelect and ShopsConnectButtons components  * Added support for daily repeating jobs  * Created MongoSyncAgent for fetching documents from the production database to the dev and beta dbs  * Added left and right LayoutMainDrawer-s for displaying information next to the main content  * Added CategorySuggest component for selecting categories from a long list  * Added ShopsMap component for mapping shop categories to standard categories (Google Product Taxonomy)  * Added Products.createdAt field  * Fixed bug where Wholesale rate of 60% was mislabelled as 55% on Supplier settings page  * Added Terms & Conditions with a link on the Sign-up page  * Created Mongo.Collection.prototype.insertMany function  * Added support for startComponent and endComponent in form groups  * Added support for form.getHidden parameter, where the schema can pass a function to control the visibility of a form field  * Refactored and simplified the vulcan-material-ui sampleTheme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'erikdakoda:vulcan-material-ui',
-  version: '0.13.9',
+  version: '0.13.10',
   summary: 'Replacement for Vulcan (http://vulcanjs.org/) components using material-ui',
   git: 'https://github.com/ErikDakoda/vulcan-material-ui',
   documentation: 'README.md'
