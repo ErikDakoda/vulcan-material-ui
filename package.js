bb2605f236e905e80fe3cdb7185a8cf582b781cb
Erik Schannen <erikschannen@gmail.com>
2018-04-05 12:53:49 -0400

    * Fixed various minor bugs found in beta testing  * Removed search box on MyLists page  * Clarified empty list message for shop lists

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'erikdakoda:vulcan-material-ui',
-  version: '0.14.3',
+  version: '0.14.4',
   summary: 'Replacement for Vulcan (http://vulcanjs.org/) components using material-ui',
   git: 'https://github.com/ErikDakoda/vulcan-material-ui',
   documentation: 'README.md'
