229ca026eb2ed7d944c9b57bab02d2b23851716b
Erik Schannen <erikschannen@gmail.com>
2018-04-15 18:36:31 -0400

    0.14.7 / 2018-04-15 ===================
     * Added the clear button back on the search input
     * Changed the default name of new companies to "Company Name"
     * Added Company approval process
     * Added Welcome Wizard on Company settings page
     * Added request and approval emails
     * Changed Company settings page to use Company slug instead of ID

--- components/bonus/SearchInput.jsx
+++ components/bonus/SearchInput.jsx
@@ -11,30 +11,56 @@ import classNames from 'classnames';
 
 
 const styles = theme => ({
+  '@global': {
+    'input[type=text]::-ms-clear, input[type=text]::-ms-reveal':
+      {
+        display: 'none',
+        width: 0,
+        height: 0,
+      },
+    'input[type="search"]::-webkit-search-decoration, input[type="search"]::-webkit-search-cancel-button':
+      { display: 'none' },
+    'input[type="search"]::-webkit-search-results-button, input[type="search"]::-webkit-search-results-decoration':
+      { display: 'none' },
+  },
   root: {
     textAlign: 'center',
-  },
-  dense: {
-    '& $search': {
-      padding: '4px 8px',
-      width: '180px',
-    },
-    /*'& $clear': {
-      width: '32px',
-      height: '32px',
-      margin: '-2px -2px -2px 0',
-    }*/
   },
   search: {
     display: 'inline-flex',
     flexDirection: 'row',
     alignItems: 'center',
-    width: '200px',
+    width: 220,
+    padding: 6,
     backgroundColor: theme.palette.common.faintBlack,
-    borderRadius: '18px',
-    padding: '8px 16px',
+    borderRadius: 20,
     '&:focus': {
       backgroundColor: theme.palette.common.darkBlack,
+    }
+  },
+  clear: {
+    transition: theme.transitions.create('opacity', {
+      duration: theme.transitions.duration.short,
+    }),
+    opacity: 0.65,
+    width: 36,
+    height: 36,
+    margin: -6,
+    marginLeft: 0,
+    '& svg': {
+      width: 16
+    },
+  },
+  dense: {
+    '& $search': {
+      width: 200,
+      padding: 4,
+    },
+    '& $clear': {
+      width: 32,
+      height: 32,
+      margin: -4,
+      marginLeft: 0,
     }
   },
   icon: {
@@ -45,79 +71,68 @@ const styles = theme => ({
   input: {
     lineHeight: 1,
     padding: 0,
-  },
-  /*clear: {
-    transition: theme.transitions.create('opacity', {
-      duration: theme.transitions.duration.short,
-    }),
-    opacity: '0.65',
-    width: '36px',
-    height: '36px',
-    margin: '-4px -4px -4px 0',
-    '& svg': {
-      width: '16px'
-    },
+    marginBottom: 1,
   },
   clearDisabled: {
     opacity: 0,
-  }*/
+  }
 });
 
 
 class SearchInput extends PureComponent {
-
+  
   constructor (props) {
     super(props);
-
+    
     this.state = {
       value: '',
     };
-
+    
     this.focusInput = this.focusInput.bind(this);
-    //this.clearSearch = this.clearSearch.bind(this);
+    this.clearSearch = this.clearSearch.bind(this);
     this.updateSearch = this.updateSearch.bind(this);
     this.updateQuery = this.updateQuery.bind(this);
     this.updateQuery = _.debounce(this.updateQuery, 500);
   }
-
+  
   focusInput () {
     this.input.focus();
   }
-
-  /*clearSearch () {
+  
+  clearSearch () {
     this.setState({
       value: '',
     });
     this.updateQuery('');
-  }*/
-
+  }
+  
   updateSearch (event) {
     //event.preventDefault();
-
+    
     const value = event.target.value;
     this.setState({
       value: value
     });
     this.updateQuery(value);
   }
-
+  
   updateQuery (value) {
     this.props.updateQuery(value);
   }
-
+  
   render () {
     const {
       classes,
       className,
       dense
     } = this.props;
-
+    
     return (
       <div className={classNames('search-input', classes.root, className, dense && classes.dense)}>
         <FormControl className={classes.search}>
-
+          
           <SearchIcon className={classes.icon} onClick={this.focusInput}/>
-
+          
           <Input className={classNames('search-input')}
                  classes={{ input: classes.input }}
                  id="search-input"
@@ -127,19 +142,20 @@ class SearchInput extends PureComponent {
                  onChange={this.updateSearch}
                  disableUnderline={true}
           />
-
-          {/*<IconButton className={classNames(classes.clear, !this.state.value && classes.clearDisabled)}
-                      onClick={this.clearSearch}
-                      disabled={!this.state.value}
+          
+          <IconButton
+            className={classNames(classes.clear, !this.state.value && classes.clearDisabled)}
+            onClick={this.clearSearch}
+            disabled={!this.state.value}
           >
             <ClearIcon/>
-          </IconButton>*/}
-
+          </IconButton>
+        
         </FormControl>
       </div>
     );
   }
-
+  
 }
 
 
