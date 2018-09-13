d82e070738d7f7faf5a156f5a904fe34e997b146
Eric Burel <e.burel.contact@gmail.com>
2018-09-13 09:56:45 +0200

    fix layout height (padding was ignored)

--- example/Layout.jsx
+++ example/Layout.jsx
@@ -12,121 +12,124 @@ const drawerWidth = 240;
 const topBarHeight = 100;
 
 
-const styles = theme => ({
-  '@global': {
-    html: {
-      background: theme.palette.background.default,
-      WebkitFontSmoothing: 'antialiased',
-      MozOsxFontSmoothing: 'grayscale',
+const styles = theme => {
+  const contentPadding = theme.spacing.unit * 8
+
+  return {
+    '@global': {
+      html: {
+        background: theme.palette.background.default,
+        WebkitFontSmoothing: 'antialiased',
+        MozOsxFontSmoothing: 'grayscale',
+        overflow: 'hidden',
+      },
+      body: {
+        margin: 0,
+      },
+    },
+    root: {
+      width: '100%',
+      zIndex: 1,
       overflow: 'hidden',
     },
-    body: {
-      margin: 0,
+    appFrame: {
+      position: 'relative',
+      display: 'flex',
+      height: '100vh',
+      alignItems: 'stretch',
     },
-  },
-  root: {
-    width: '100%',
-    zIndex: 1,
-    overflow: 'hidden',
-  },
-  appFrame: {
-    position: 'relative',
-    display: 'flex',
-    height: '100vh',
-    alignItems: 'stretch',
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
-    backgroundColor: theme.palette.background[200],
-  },
-  drawerHeader: {
-    height: `${topBarHeight}px !important`,
-    minHeight: `${topBarHeight}px !important`,
-    position: 'relative !important',
-  },
-  content: {
-    padding: theme.spacing.unit * 8,
-    width: '100%',
-    marginLeft: -drawerWidth,
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    color: theme.palette.text.primary,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    height: `calc(100% - ${topBarHeight}px)`,
-    marginTop: topBarHeight,
-    overflowY: 'scroll',
-  },
-  mainShift: {
-    marginLeft: 0,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-});
+    drawerPaper: {
+      position: 'relative',
+      width: drawerWidth,
+      backgroundColor: theme.palette.background[200],
+    },
+    drawerHeader: {
+      height: `${topBarHeight}px !important`,
+      minHeight: `${topBarHeight}px !important`,
+      position: 'relative !important',
+    },
+    content: {
+      padding: contentPadding,
+      width: '100%',
+      marginLeft: -drawerWidth,
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      color: theme.palette.text.primary,
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      height: `calc(100% - ${topBarHeight}px - ${contentPadding * 2}px)`,
+      marginTop: topBarHeight,
+      overflowY: 'scroll',
+    },
+    mainShift: {
+      marginLeft: 0,
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+  });
 
 
-class Layout extends React.Component {
-  state = {
-    isOpen: { sideNav: true }
-  };
-  
-  toggle = (item, openOrClose) => {
-    const newState = { isOpen: {} };
-    newState.isOpen[item] = typeof openOrClose === 'string' ?
-      openOrClose === 'open' :
-      !this.state.isOpen[item];
-    this.setState(newState);
-  };
-  
-  render = () => {
-    const routeName = Utils.slugify(this.props.currentRoute.name);
-    const classes = this.props.classes;
-    const isOpen = this.state.isOpen;
-    
-    return (
-      <div className={classNames(classes.root, 'wrapper', `wrapper-${routeName}`)}>
-        <div className={classes.appFrame}>
-          
-          <Components.Header isSideNavOpen={isOpen.sideNav}
-                             toggleSideNav={openOrClose =>
-                               this.toggle('sideNav', openOrClose)}/>
-          
-          <Drawer variant="persistent"
-                  classes={{ paper: classes.drawerPaper, }}
-                  open={isOpen.sideNav}
-          >
-            <AppBar className={classes.drawerHeader} elevation={4} square={true}>
-              <Toolbar>
-              </Toolbar>
-            </AppBar>
-            <Components.SideNavigation/>
-          </Drawer>
-          
-          <main className={classNames(classes.content, isOpen.sideNav && classes.mainShift)}>
-            {this.props.children}
-          </main>
-          
-          <Components.FlashMessages/>
-        
+  class Layout extends React.Component {
+    state = {
+      isOpen: { sideNav: true }
+    };
+
+    toggle = (item, openOrClose) => {
+      const newState = { isOpen: {} };
+      newState.isOpen[item] = typeof openOrClose === 'string' ?
+        openOrClose === 'open' :
+        !this.state.isOpen[item];
+      this.setState(newState);
+    };
+
+    render = () => {
+      const routeName = Utils.slugify(this.props.currentRoute.name);
+      const classes = this.props.classes;
+      const isOpen = this.state.isOpen;
+
+      return (
+        <div className={classNames(classes.root, 'wrapper', `wrapper-${routeName}`)}>
+          <div className={classes.appFrame}>
+
+            <Components.Header isSideNavOpen={isOpen.sideNav}
+              toggleSideNav={openOrClose =>
+                this.toggle('sideNav', openOrClose)} />
+
+            <Drawer variant="persistent"
+              classes={{ paper: classes.drawerPaper, }}
+              open={isOpen.sideNav}
+            >
+              <AppBar className={classes.drawerHeader} elevation={4} square={true}>
+                <Toolbar>
+                </Toolbar>
+              </AppBar>
+              <Components.SideNavigation />
+            </Drawer>
+
+            <main className={classNames(classes.content, isOpen.sideNav && classes.mainShift)}>
+              {this.props.children}
+            </main>
+
+            <Components.FlashMessages />
+
+          </div>
         </div>
-      </div>
-    );
+      );
+    };
+  }
+
+
+  Layout.propTypes = {
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.node,
   };
-}
 
 
-Layout.propTypes = {
-  classes: PropTypes.object.isRequired,
-  children: PropTypes.node,
-};
+  Layout.displayName = 'Layout';
 
 
-Layout.displayName = 'Layout';
-
-
-replaceComponent('Layout', Layout, [withStyles, styles]);
+  replaceComponent('Layout', Layout, [withStyles, styles]);
