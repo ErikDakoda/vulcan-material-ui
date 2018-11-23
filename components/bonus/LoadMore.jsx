97bb584be5afd0f214fe2e9ceacd14ab8d5c6f3d
Erik Dakoda <erik@qeebi.com>
2018-11-23 17:43:29 -0500

    1.12.8_8 / 2018-11-23 =====================
     * Improved the functionality of the LoadMore component
       * The showNoMore property has been deprecated
       * A showCount property has been added (true by default) that shows a count of loaded and total items
       * The load more icon or button is displayed even when infiniteScroll is enabled

--- components/bonus/LoadMore.jsx
+++ components/bonus/LoadMore.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, intlShape } from 'meteor/vulcan:i18n';
+import { FormattedMessage, intlShape } from 'react-intl';
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 import ArrowDownIcon from 'mdi-material-ui/ArrowDown';
-import MoreIcon from 'mdi-material-ui/More';
 import ScrollTrigger from './ScrollTrigger';
 import classNames from 'classnames';
 
 
 const styles = theme => ({
+  
   root: {
     textAlign: 'center',
     flexBasis: '100%',
   },
+  
   textButton: {
     marginTop: theme.spacing.unit * 2,
   },
+  
   iconButton: {},
+  
   caption: {
+    marginTop: theme.spacing.unit * 3,
     paddingTop: theme.spacing.unit,
     paddingBottom: theme.spacing.unit,
   },
-  tooltip: {
-    margin: 0,
-  }
+  
 });
 
 
@@ -38,18 +39,37 @@ const LoadMore = ({
                     totalCount,
                     loadMore,
                     networkStatus,
-                    showNoMore,
+                    showCount,
                     useTextButton,
                     className,
                     infiniteScroll,
                   }, { intl }) => {
   
   const isLoadingMore = networkStatus === 2;
-  const loadMoreText = intl.formatMessage({ id: 'global.load_more' });
+  const loadMoreText = intl.formatMessage({ id: 'load_more.load_more' });
   const title = `${loadMoreText} (${count}/${totalCount})`;
+  const hasMore = totalCount > count;
+  const countValues = { count, totalCount };
+  
+  const loadMoreButton = useTextButton
+    ?
+    <Button className={classes.textButton} onClick={() => loadMore()}>
+      {title}
+    </Button>
+    :
+    <IconButton className={classes.iconButton} onClick={() => loadMore()}>
+      <ArrowDownIcon/>
+    </IconButton>;
   
   return (
     <div className={classNames('load-more', classes.root, className)}>
+      {
+        showCount &&
+        
+        <Typography variant="caption" className={classes.caption}>
+          <FormattedMessage id={`load_more.${hasMore ? 'loaded_count' : 'loaded_all'}`} values={countValues}/>
+        </Typography>
+      }
       {
         isLoadingMore
           
@@ -59,7 +79,7 @@ const LoadMore = ({
           
           :
           
-          totalCount > count
+          hasMore
             
             ?
             
@@ -68,41 +88,15 @@ const LoadMore = ({
               ?
               
               <ScrollTrigger onEnter={() => loadMore()}>
-                <MoreIcon/>
+                {loadMoreButton}
               </ScrollTrigger>
               
               :
               
-              useTextButton
-                
-                ?
-                
-                <Button className={classes.textButton} onClick={() => loadMore()}>
-                  {title}
-                </Button>
-                
-                :
-                
-                <Tooltip id="tooltip-more"
-                         classes={{ tooltip: classes.tooltip }}
-                         title={title}
-                         placement="top"
-                >
-                  <IconButton className={classes.iconButton} onClick={() => loadMore()}>
-                    <ArrowDownIcon/>
-                  </IconButton>
-                </Tooltip>
-            
+              loadMoreButton
             :
-            
-            showNoMore &&
-            
-            <Typography variant="caption" className={classes.caption}>
-              <FormattedMessage
-                id="global.no_more"
-                defaultMessage="No more items"
-              />
-            </Typography>
+  
+            null
       }
     </div>
   );
@@ -115,10 +109,15 @@ LoadMore.propTypes = {
   totalCount: PropTypes.number,
   loadMore: PropTypes.func,
   networkStatus: PropTypes.number,
-  showNoMore: PropTypes.bool,
+  showCount: PropTypes.bool,
   useTextButton: PropTypes.bool,
   className: PropTypes.string,
   infiniteScroll: PropTypes.bool,
+};
+
+
+LoadMore.defaultProps = {
+  showCount: true,
 };
 
 
--- history.md
+++ history.md
@@ -1,3 +1,11 @@
+1.12.8_8 / 2018-11-23
+=====================
+
+ * Improved the functionality of the LoadMore component
+   * The showNoMore property has been deprecated
+   * A showCount property has been added (true by default) that shows a count of loaded and total items
+   * The load more icon or button is displayed even when infiniteScroll is enabled
+ 
 1.12.8_7 / 2018-11-10
 =====================
 
